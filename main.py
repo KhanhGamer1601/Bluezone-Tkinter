694028c75ba3a0117bbf51608c3aac9e31cfecc5
KhanhGamer1601 <KhanhGamer1601@gmail.com>
2021-04-18 17:49:03 +0700

    'App'

--- main.py
+++ main.py
@@ -30,22 +30,28 @@ class App:
         self.Month_List = [
             i for i in range(1, 13)
         ]
+        self.Year_List = [
+            i for i in range(1900, 2022)
+        ]
 
         self.Date_Var = IntVar()
         self.Month_Var = IntVar()
+        self.Year_Var = IntVar()
 
         self.Date_Label = Label(self.app, text = 'Date you were born: ').place(x = 150, y = 100)
         self.Month_Label = Label(self.app, text = 'Month you were born: ').place(x = 150, y = 150)
+        self.Year_Label = Label(self.app, text = 'Year you were born: ').place(x = 150, y = 200)
 
         self.Date = OptionMenu(self.app, self.Date_Var, *self.Date_List).place(x = 280, y = 100)
         self.Month = OptionMenu(self.app, self.Month_Var, *self.Month_List).place(x = 280, y = 150)
+        self.Year = OptionMenu(self.app, self.Year_Var, *self.Year_List).place(x = 280, y = 200)
 
         self.Gender = IntVar()
-        self.Male = Radiobutton(self.app, text = 'Male', variable = self.Gender, value = 1).place(x = 150, y = 200)
-        self.Female = Radiobutton(self.app, text = 'Female', variable = self.Gender, value = 2).place(x = 230, y = 200)
+        self.Male = Radiobutton(self.app, text = 'Male', variable = self.Gender, value = 1).place(x = 150, y = 250)
+        self.Female = Radiobutton(self.app, text = 'Female', variable = self.Gender, value = 2).place(x = 230, y = 250)
 
         self.Show_Datetime = Label(self.app, text = 'Time: ')
-        self.Show_Datetime.place(x = 150, y = 250)
+        self.Show_Datetime.place(x = 150, y = 300)
         self.UpdateTime()
 
         self.Countries = [
@@ -56,9 +62,9 @@ class App:
         ]
 
         self.Nationality_var = StringVar()
-        self.Nationality_Label = Label(self.app, text = 'Nationality: ').place(x = 150, y = 300)
+        self.Nationality_Label = Label(self.app, text = 'Nationality: ').place(x = 150, y = 350)
         self.Nationality = OptionMenu(self.app, self.Nationality_var, *self.Countries)
-        self.Nationality.place(x = 270, y = 300)
+        self.Nationality.place(x = 270, y = 350)
 
         self.User_var = StringVar()
         self.User_Target_Var = StringVar()
@@ -67,8 +73,15 @@ class App:
         self.User_Position_Entry = None
         self.User_Target_Entry = None
         self.User_Health_Entry = None
+        
+        self.Health_List = [
+            'cough',
+            'sneeze',
+            'snivel',
+            'shortness of breath',
+        ]
 
-        self.Next = Button(self.app, text = 'Next', width = 16, command = self.Next).place(x = 150, y = 350)
+        self.Next = Button(self.app, text = 'Next', width = 16, command = self.Next).place(x = 150, y = 400)
 
         self.app.config(menu = self.main_menu)
         self.app.mainloop()
@@ -89,12 +102,15 @@ class App:
                 return messagebox.showinfo('notification', 'You have forgotten to choose the month !')
             if self.Date_Var.get() == 30 or self.Date_Var.get() == 31 and self.Month_Var.get() == 2:
                 return messagebox.showinfo('notification', 'Does not exist !')
+            if self.Year_Var.get() == 0:
+                return messagebox.showinfo('notification', 'You have forgotten to choose the year !')
             else:
                 self.HandleUsername[0].configure(bg = 'white')
             Save = open(self.Username_Data.get(), 'a')
             Save.write(self.Username_Data.get() + '\n')
             Save.write(str(self.Date_Var.get()) + '\n')
             Save.write(str(self.Month_Var.get()) + '\n')
+            Save.write(str(self.Year_Var.get()) + '\n')
             Save.write(str(self.Gender.get()) + '\n')
             Save.write(self.Nationality_var.get() + '\n')
         except:
@@ -113,7 +129,7 @@ class App:
         self.User_Target_Entry.place(x = 270, y = 100)
 
         self.User_Health = Label(page, text = 'Your health: ').place(x = 150, y = 150)
-        self.User_Health_Entry = Entry(page, textvariable = self.User_Health_Var)
+        self.User_Health_Entry = OptionMenu(page, self.User_Health_Var, *self.Health_List)
         self.User_Health_Entry.place(x = 270, y = 150)
 
         Submit = Button(page, text = 'Submit', width = 16, command = self.Bluezone_Submit).place(x = 150, y = 200)
